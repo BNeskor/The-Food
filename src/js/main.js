7b70abd9e0513040045b367b01f307e92459f198
Bohdan <bogdaneskor@gmail.com>
2021-10-25 22:00:33 +0300

    first comit

---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+window.addEventListener('DOMContentLoaded',()=>{
+   const parent = document.querySelector('.tabheader__items'),
+         tabs = document.querySelectorAll('.tabheader__item'),
+         items = document.querySelectorAll('.tabcontent');
+
+   function hide(){
+      tabs.forEach(item => {
+         item.classList.remove('tabheader__item_active');
+      });
+      items.forEach(item =>{
+         item.classList.remove('tabcontent-active');
+      });
+   }
+   function show(i = 0){
+      tabs[i].classList.add('tabheader__item_active');
+      items[i].classList.add('tabcontent-active');
+   }
+
+   parent.addEventListener('click',(e)=>{
+      const target = e.target;
+      if(target && target.classList.contains("tabheader__item")){
+         tabs.forEach((item, i)=>{
+            if(target == item){
+               hide();
+               show(i);
+            }
+         });
+      }
+   });
+});
