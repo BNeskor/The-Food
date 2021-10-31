9bbb732a196f328f2ac822703210354fce2fcbfe
Bohdan <bogdaneskor@gmail.com>
2021-10-31 19:47:57 +0200

    add JS Class for creating menu carts

--- src/js/main.js
+++ src/js/main.js
@@ -107,25 +107,94 @@ window.addEventListener('DOMContentLoaded', () => {
          toggleModal();
       }
    });
-   document.addEventListener('keydown',(e)=>{
+   document.addEventListener('keydown', (e) => {
       if (e.code === "Escape" && modal.classList.contains('show')) {
          toggleModal();
       }
    });
    document.addEventListener('scroll', showModalByscroll);
-   
+
    function toggleModal() {
       modal.classList.toggle('show');
       document.body.classList.toggle("scroll");//bocumnet overflow - hidden  or scroll
       clearTimeout(modalTimeout);
-      document.removeEventListener('scroll',showModalByscroll);
+      document.removeEventListener('scroll', showModalByscroll);
    }
 
-   function showModalByscroll(){
-      if(document.body.offsetHeight == window.scrollY + document.documentElement.clientHeight ){
+   function showModalByscroll() {
+      if (document.body.offsetHeight == window.scrollY + document.documentElement.clientHeight) {
          toggleModal();
       }
    }
    //MODAL END
+
+
+   //MENU
+
+
+   class MenuCart {
+      constructor(src, alt, title, descr, price, parentSelector) {
+         this.src = src;
+         this.alt = alt;
+         this.title = title;
+         this.descr = descr;
+         this.price = price;
+         this.transfer = 27;
+         this.parent = document.querySelector(parentSelector);
+         this.changeToUAH();
+      }
+
+      changeToUAH() {
+         this.price = this.price * this.transfer;
+      }
+      render() {
+         const element = document.createElement('div');
+         element.innerHTML = `
+         <div class="menu__item">
+             <img src=${this.src} alt=${this.alt}>
+               <h3 class="menu__item-subtitle">${this.title}</h3>
+               <div class="menu__item-descr">${this.descr}</div>
+               <div class="menu__item-divider"></div>
+               <div class="menu__item-price">
+                  <div class="menu__item-cost">Цена:</div>
+                  <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+               </div>
+         </div>`;
+         this.parent.append(element);
+      }
+   }
+
+   new MenuCart(
+      "img/tabs/elite.jpg",
+      "elite",
+      'Меню “Премиум”',
+      'В меню “Премиум” мы используем не только красивый дизайн упаковки,\
+      но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+      12,
+      ".menu .container"
+   ).render();
+   new MenuCart(
+      "img/tabs/vegy.jpg",
+      "vegy",
+      'Меню "Фитнес"',
+      'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих\
+      овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной\
+      ценой и высоким качеством!',
+      5,
+      ".menu .container"
+   ).render();
+   new MenuCart(
+      "img/tabs/post.jpg",
+      "post",
+      'Меню "Постное"',
+      'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие\
+      продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное\
+      количество белков за счет тофу и импортных вегетарианских стейков. ',
+      9,
+      ".menu .container"
+   ).render();
+
+   
+   //MENU END
 
 });
